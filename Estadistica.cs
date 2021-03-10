d1e5f8c64c31dddc02cfd3653e2fb93258b721f9
Ludwig Escalante García <ludwig.escalante@gnp.com.mx>
2021-03-10 13:15:16 -0600

    Add project files.

---
+++ Estadistica.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Actividad4
+{
+    class Estadistica
+    {
+
+        public int[] arreNum = new int[10];
+
+        public Estadistica()
+        {
+            arreNum = readData();
+        }
+
+        public int[] readData()
+        {
+            int[] arreAux = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Ingresa un número (" + i + "): " );
+                arreAux[i] = Convert.ToInt16(Console.ReadLine());
+            }
+            return arreAux;
+        }
+
+
+        public void writeData(int[] x)
+        {
+            for(int i = 0; i < x.Length; i++)
+            {
+                Console.WriteLine(x[i]);
+            }
+        }
+
+
+        public float calcMedia(int[] x)
+        {
+            float sum = 0;
+            for(int i=0; i < x.Length; i++)
+            {
+                sum += x[i];
+            }
+            return (sum / x.Length);
+        }
+
+        //public int calcMin(int[] x)
+        //{
+        //    ;
+        //}
+
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Actividad4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Estadistica ob1 = new Estadistica();
+            ob1.writeData(ob1.arreNum);
+            Console.WriteLine("La media es: " + ob1.calcMedia(ob1.arreNum));
+        }
+    }
+}
